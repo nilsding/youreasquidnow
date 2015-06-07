3bd7c102028c4d3e2efa5f522117b831b2ef58b2
Jona H. <jona@schisma.co>
2015-06-07 19:03:59 +0200

    WHOOPS

--- java/java.java
+++ java/java.java
@@ -1,5 +1,4 @@
 public static void main (String args[]) {
-  String[] you = {"squ", "k"};
   while (true) {
     System.out.println("You're a kid now!");
     System.out.println("You're a squid now!");
