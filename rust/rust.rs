f4619b4f6a5cf98e55d94e513f7d019dae357209
schisma <jona@schisma.co>
2015-06-07 14:48:44 +0200

    fixed rust thing

--- rust/rust.rs
+++ rust/rust.rs
@@ -1,5 +1,7 @@
-loop {
-  for i in vec!("squ", "k") {
-    println!("You're a {}id now!", i);
+fn main () {
+  loop {
+    for i in vec!("squ", "k") {
+      println!("You're a {}id now!", i);
+    }
   }
 }
