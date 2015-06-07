65b0df65ddc22bc0693db4ae2f072fdeb644a4c7
schisma <jona@schisma.co>
2015-06-07 16:24:39 +0200

    added java

---
+++ java/java.java
@@ -0,0 +1,8 @@
+public static void main (String args[]) {
+  String[] you = {"squ", "k"};
+  while (true) {
+    for (String s: you) {
+      System.out.println("You're a " + s + "id now!");
+    }
+  }
+}
