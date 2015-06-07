2bba7809a9f53bb49905fef5cec2b5f1fbb5de7a
Robin B. <lordrob1nn@googlemail.com>
2015-06-07 15:56:31 +0200

    fuck

--- php/php.php
+++ php/php.php
@@ -2,7 +2,7 @@
 	      			  $php = array('k', 'squ');
   	    	 	 	 	   while (true) {
 	  	    	 	 	    	   foreach ($php as $sucks) {
-  		 								   echo "You're a " . $sucks . "id now"+PHP_EOL;
+  		 								   echo "You're a " . $sucks . "id now" . PHP_EOL;
                 			           }
 			   	           };
 ?>
