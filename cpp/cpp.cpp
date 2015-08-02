aec50ad9abdc400bccbf9b94f127178dbf86c81a
l-a-i-n <lain@lain.org.uk>
2015-08-02 02:11:57 +0100

    Updated C++ example to use range-based for, type deduction, and remove dependancy on the algorithm header.

--- cpp/cpp.cpp
+++ cpp/cpp.cpp
@@ -1,15 +1,16 @@
 // build with: clang++ -std=c++11 -o squid cpp.cpp
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 int main(int argc, char** argv)
 {
-    std::vector<std::string> you = {"k", "squ"};
-    while (true) {
-        for_each(you.begin(), you.end(), [](std::string x) {
-            std::cout << "You're a " << x << "id now!" << std::endl;
-        });
+  std::vector<std::string> you = {"k", "squ"};
+  
+  while (true) {
+    for(auto& s: you) {
+      std::cout << "You're a " << s << "id now!" << std::endl;
     }
-    return 0;
+  }   
+  
+  return 0;
 }
