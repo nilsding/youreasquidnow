1f4c995ae18c90b030952566dc4169f015dfaff1
Rajendra Gujja <it.rajendra@gmail.com>
2015-09-17 18:07:41 -0700

    Scala version

--- scala/Squid.scala
+++ scala/Squid.scala
@@ -2,7 +2,7 @@ object Squid {
   val squids = List("k", "squ");
 
   def squid : Unit = {
-    squids.foreach(s => println(String.format("You are %sid now", s)))
+    squids.foreach(s => println(String.format("You are %sid now!", s)))
     squid
   }
 }
