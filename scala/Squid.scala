b29b3fdc796b47800007d79b3903dcbf21bcc9ed
Rajendra Gujja <it.rajendra@gmail.com>
2015-09-18 10:06:09 -0700

    Corrected the squid phrase

--- scala/Squid.scala
+++ scala/Squid.scala
@@ -1,8 +1,8 @@
 object Squid {
-  val squids = List("k", "squ");
+  val squids = List("k", "squ")
 
   def squid : Unit = {
-    squids.foreach(s => println(String.format("You are %sid now!", s)))
+    squids.foreach(s => println(String.format("You're a %sid now!", s)))
     squid
   }
 }
