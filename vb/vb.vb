c7c7665f67b372e0aa3180dc44d1cf0fe11b3108
Jona H. <jona@schisma.co>
2015-10-02 12:46:43 +0200

    Update vb.vb

--- vb/vb.vb
+++ vb/vb.vb
@@ -2,7 +2,7 @@ Module Squid
     Sub Main()
         Dim squid As Boolean = False
         Do
-            Dim str As String = iif(squid = True, "squ", "k")
+            Dim str As String = iif(squid == True, "squ", "k")
             Console.WriteLine("You're a " + str + "id now!")
             squid = not squid
         Loop
