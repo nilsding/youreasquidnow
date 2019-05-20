3186f8dfca789348c8ae1cafc01c5078dc2c5710
Thomas <9749173+uhthomas@users.noreply.github.com>
2019-05-20 22:49:24 +0100

    Update go (#3)
    * update go
    
    * simplify go
    
    * remove test code from go

--- go/go.go
+++ go/go.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
- a := []string{"k", "squ"}
- for {
-  for _, v := range a {
-   fmt.Printf("You're a %sid now!\n", val)
-  }
- }
+	const s = "ksqu"
+	for i := 0; ; i = (i + 1) % 2 {
+		fmt.Printf("You're a %sid now!\n", s[i:1+i*3])
+	}
 }
