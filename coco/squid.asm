fbadd7b13de824fbb5b982bdb43f36b615e57661
Dimitri Theulings <dimitri@tastypies.co.uk>
2019-08-02 08:45:01 +0100

    Code improvement (#5)
    * code improvement

--- coco/squid.asm
+++ coco/squid.asm
@@ -1,18 +1,16 @@
 you     equ     $a002
         org     $3f00
-        bra     squid
-are     ldx     #now
+are     ldx     #squid
+        lda     9,x
+        eora    #$18
+        sta     9,x
 a       lda     ,x+
-        beq     squid
+        beq     are
         jsr     [you]
         cmpa    #$4b
         bne     a
         lda     ,x++
         bra     a
-squid   lda     now+9
-        eora    #$18
-        sta     now+9
-        bra     are
-now     fcc     /YOU'RE A SQUID NOW!/
-        fdb     $0d00
+squid   fcc     /YOU'RE A SQUID NOW!/
+now     fdb     $0d00
         end
